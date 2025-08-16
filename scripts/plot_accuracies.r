73f540cd6adc0248460cb858189fbe03f848b7c2
Martijn de Vos <mdmartijndevos@gmail.com>
2025-08-16 14:51:25 +0200

    New plots

--- scripts/plot_accuracies.r
+++ scripts/plot_accuracies.r
@@ -1,6 +1,6 @@
 library(ggplot2)
 
-for (dataset in c("newsgroups", "emotion")) {
+for (dataset in c("newsgroups", "emotion", "wikitext")) {
   dat <- read.csv(paste0("../data/accuracies_n64_", dataset, ".csv"))
 
   ggplot(dat, aes(x=round, y=accuracy, group=group, color=group)) +
@@ -12,5 +12,15 @@ for (dataset in c("newsgroups", "emotion")) {
     labs(color="Algorithm")
 
   ggsave(paste0("../plots/accuracies_n64_", dataset, ".pdf"), width=4.3, height=2.3)
-}
 
+  # Loss
+  ggplot(dat, aes(x=round, y=loss, group=group, color=group)) +
+    geom_line() +
+    xlab("Round") +
+    ylab("Loss") +
+    theme_bw() +
+    theme(legend.position=c(0.8, 0.45), legend.background = element_rect(color = "black", fill = "white", size = 0.3)) +
+    labs(color="Algorithm")
+
+  ggsave(paste0("../plots/loss_n64_", dataset, ".pdf"), width=4.3, height=2.3)
+}
